771c4b17c3504bff543f7627a51376cedcff81be
Glareone <enlarius@gmail.com>
2023-08-07 11:37:15 +0300

    chapter 2, editorconfig, registration using reflection, little cleanup

---
+++ Chapters/MinimalAPI/Chapter02/Chapter02-Architecturing-minimal-api/PeopleHelper.cs
@@ -0,0 +1,38 @@
+﻿namespace Chapter02_Architecturing_minimal_api;
+
+public class PeopleHelper : IEndpointRouteHandler
+{
+    public void MapEndpoints(IEndpointRouteBuilder
+        app)
+    {
+        app.MapGet("/api/people", GetList);
+        app.MapGet("/api/people/{id:guid}", Get);
+        app.MapPost("/api/people", Insert);
+        app.MapPut("/api/people/{id:guid}", Update);
+        app.MapDelete("/api/people/{id:guid}", Delete);
+    }
+
+    private static IResult GetList(PeopleService peopleService)
+    {
+        return Results.Ok(); }
+
+    private static IResult Get(Guid id, PeopleService
+        peopleService)
+    {
+        return Results.Ok(); }
+
+    private static IResult Insert(Person person,
+        PeopleService people)
+    {
+        return Results.Ok(); }
+
+    private static IResult Update(Guid id, Person
+        person, PeopleService people)
+    {
+        return Results.Ok(); }
+
+    private static IResult Delete(Guid id)
+    {
+        return Results.Ok(true);
+    }
+}
---
+++ Chapters/MinimalAPI/Chapter02/Chapter02-Architecturing-minimal-api/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+using Chapter02_Architecturing_minimal_api;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<PeopleService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// register all endpoints using reflection
+app.MapAllEndpoints(Assembly.GetExecutingAssembly());
+
+app.UseHttpsRedirection();
+
+app.Run();
