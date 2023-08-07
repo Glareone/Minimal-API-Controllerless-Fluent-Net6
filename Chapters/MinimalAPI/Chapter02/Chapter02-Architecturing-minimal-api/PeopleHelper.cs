14125a8cb7d325dc03e6381a599bbc8cdcc0ece2
Glareone <enlarius@gmail.com>
2023-08-07 12:41:40 +0300

    swagger configuration, additional attributes to endpoints

--- Chapters/MinimalAPI/Chapter02/Chapter02-Architecturing-minimal-api/PeopleHelper.cs
+++ Chapters/MinimalAPI/Chapter02/Chapter02-Architecturing-minimal-api/PeopleHelper.cs
@@ -1,15 +1,26 @@
-﻿namespace Chapter02_Architecturing_minimal_api;
+﻿using System.Net.Mime;
+
+namespace Chapter02_Architecturing_minimal_api;
 
 public class PeopleHelper : IEndpointRouteHandler
 {
     public void MapEndpoints(IEndpointRouteBuilder
         app)
     {
-        app.MapGet("/api/people", GetList);
+        app.MapGet("/api/people", GetList)
+            .Produces<ResponseData>(StatusCodes.Status200OK)
+            .WithTags("Endpoint Sample")
+            .WithName("GetUserListOperation"); // Operation Ids to OpenAPI
+
         app.MapGet("/api/people/{id:guid}", Get);
-        app.MapPost("/api/people", Insert);
+        
+        app.MapPost("/api/people", Insert)
+            .Accepts<ResponseData>(MediaTypeNames.Application.Json);
+
         app.MapPut("/api/people/{id:guid}", Update);
-        app.MapDelete("/api/people/{id:guid}", Delete);
+        
+        app.MapDelete("/api/people/{id:guid}", Delete)
+            .ExcludeFromDescription(); // Excluded from Swagger Description
     }
 
     private static IResult GetList(PeopleService peopleService)
--- Chapters/MinimalAPI/Chapter02/Chapter02-Architecturing-minimal-api/Program.cs
+++ Chapters/MinimalAPI/Chapter02/Chapter02-Architecturing-minimal-api/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System.Reflection;
 using Chapter02_Architecturing_minimal_api;
+using Chapter02_Architecturing_minimal_api.OperationFilters;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.OperationFilter<CorrelationIdOperationFilter>();
+
+    // Swagger versioning
+    c.SwaggerDoc("v1", new()
+    {
+        Title = builder.Environment.ApplicationName,
+        Version = "v1",
+        Contact = new()
+        {
+            Name = "Aleksei Kolesnikov",
+            Email = "my-work-email@gmail.com",
+            Url = new Uri("https://github.com/Glareone")
+        },
+        Description = "Minimal API - Chapter 02",
+        License = new Microsoft.OpenApi.Models.
+            OpenApiLicense(),
+        TermsOfService = new("https://github.com/Glareone")
+    });
+});
 
 builder.Services.AddScoped<PeopleService>();
 
