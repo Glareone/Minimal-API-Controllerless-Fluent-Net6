780c30e84a87f038487e3978438bbabf4ddd07be
Glareone <enlarius@gmail.com>
2023-08-09 18:54:03 +0300

    automapper

---
+++ Chapters/MinimalAPI/Chapter07-Mapping-AutoMapper/DTO/PersonDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Chapter07_Mapping_AutoMapper.DTO;
+
+public class PersonDto
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int Age { get; set; }
+    public string? City { get; set; }
+}
